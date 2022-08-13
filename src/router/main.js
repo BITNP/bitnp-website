70db7ad399812e341b713a4b4945167340abc314
iruhh <86449016+iruhh@users.noreply.github.com>
2022-08-13 10:47:20 +0800

    /src/views/home/home.vue -> ../views/home/home.vue

--- src/router/main.js
+++ src/router/main.js
@@ -1,8 +1,8 @@
 import {createRouter,createWebHashHistory} from 'vue-router'
 
 const routes =[
-    {path:'/',name:'home',component:()=>{import('/src/views/home/home.vue')}},
-    {path: '/admin',name:'admin' ,component:()=>import('/src/views/admin/admin.vue')}
+    {path:'/',name:'home',component:()=>{import('../views/home/home.vue')}},
+    {path: '/admin',name:'admin' ,component:()=>import('../views/admin/admin.vue')}
 ];
 
 const router = createRouter({
