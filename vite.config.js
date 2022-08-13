3de88b6957e7fbf29979798b6f69dea0ef974720
iruhh <86449016+iruhh@users.noreply.github.com>
2022-08-13 09:49:34 +0800

    now we can use '@/...' as the alias of 'src/...' though my vscode still can't recognize it but vite can.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 路径代理
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: 'views', replacement: '/src/views' },
+      { find: 'components', replacement: '/src/components' },
+    ]
+  }
 })
